267f91986c793b16d3d73726636cde888b5f5476
sagan5 <43873645+sagan5@users.noreply.github.com>
2018-10-11 21:36:52 +0300

    add galery carousel

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -1,15 +1,27 @@
-console.log("Pavyko!");
+// console.log("Pavyko!");
 
+// burger menu
+
+$(document).ready(function(){
+				$('.burger').click(function(){
+					$('.nav').toggleClass('show');
+				});
+			});
+
+// galery carousel
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+	items:1
+})
+
+// Google maps NOT WORKING???
 // function myMap(){
 // var rikis = {lat: 54.6746045, lng: 25.2804137};
 // var map = new google.maps.Map(document.getElementById('map'), {zoom: 14, center: rikis});
 // var marker = new google.maps.Marker({position: rikis, map: map});
 // }
 
-$(document).ready(function(){
-  $('.carousel').slick({
-    autoplay: true,
-    autoplaySpeed: 2000,
-    pauseOnHover: true
-  });
-});
