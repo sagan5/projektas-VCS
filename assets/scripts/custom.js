2e9be372c0ab535e8a292b9379fe6698233d2309
sagan5 <43873645+sagan5@users.noreply.github.com>
2018-10-11 16:28:42 +0300

    add banner carousel

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -1,7 +1,15 @@
 console.log("Pavyko!");
 
-function myMap() {
-var rikis = {lat: 54.6746045, lng: 25.2804137};
-var map = new google.maps.Map(document.getElementById('map'), {zoom: 14, center: rikis});
-var marker = new google.maps.Marker({position: rikis, map: map});
-}
+// function myMap(){
+// var rikis = {lat: 54.6746045, lng: 25.2804137};
+// var map = new google.maps.Map(document.getElementById('map'), {zoom: 14, center: rikis});
+// var marker = new google.maps.Marker({position: rikis, map: map});
+// }
+
+$(document).ready(function(){
+  $('.your-class').slick({
+    autoplay: true,
+    autoplaySpeed: 6000,
+    pauseOnHover: false
+  });
+});
