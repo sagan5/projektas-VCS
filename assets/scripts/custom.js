a7fae0b17a985cdf1dcbe447caf1af4f8e128479
sagan5 <43873645+sagan5@users.noreply.github.com>
2018-10-23 20:10:30 +0300

    add banner carousel
    (not complete)

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -1,26 +1,33 @@
-console.log("Pavyko!");
-
-// burger menu
+// console.log("Pavyko!");
 
 $(document).ready(function(){
-				$('.burger').click(function(){
-					$('.nav').toggleClass('show');
-				});
-				$('.menu-item').click(function(){
-					$('.nav').toggleClass('show');
-				});
-			});
 
-// galery carousel
+    // burger
+	$('.burger').click(function(){
+		$('.nav').toggleClass('show');
+	});
+	$('.menu-item').click(function(){
+		$('.nav').toggleClass('show');
+	});
 
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    autoplay: true,
-    autoplayTimeout: 3000,
-    autoplayHoverPause: true,
-	items:1
-})
+    // banner carousel
+    $('.slick-carousel').slick({
+        arrows:  false,
+        autoplay: true,
+        dots: true
+    });
 
+    // galery carousel
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        items:1
+    })
+});
+
+// google maps
 function myMap() {
     var mapCanvas = document.getElementById("map");
     var rikis = {lat: 54.6746045, lng: 25.2804137};
